741b7c55960fde39e93ce8dad66ff5ee8c9c7521
justtunde <tundejoshua098@gmail.com>
2026-07-13 13:07:09 +0100

    update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -44,7 +44,7 @@ const Signup = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://loanaptech-ijz6.onrender.com/api/auth/register", {
+      const response = await fetch("https://loanaptech-1-f4d2.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
